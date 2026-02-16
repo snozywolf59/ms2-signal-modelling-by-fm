0330ebff90a06c4a71e7729c5957994c182ad6e9
Bang <thbang727@gmail.com>
2026-02-16 21:51:03 +0700

    training ok. todo: guidance-free

--- demo-code/2d/flow_model.py
+++ demo-code/2d/flow_model.py
@@ -7,10 +7,10 @@ class MLP(nn.Module):
         super().__init__()
         self.net = nn.Sequential()
         self.net.add_module('input', nn.Linear(input_dim + 1, hidden_dim))
-        self.net.add_module('input_act', nn.SELU())
+        self.net.add_module('input_act', nn.SiLU())
         for i in range(layers - 2):
             self.net.add_module(f'hidden_{i}', nn.Linear(hidden_dim, hidden_dim))
-            self.net.add_module(f'hidden_act_{i}', nn.SELU())
+            self.net.add_module(f'hidden_act_{i}', nn.SiLU())
         self.net.add_module('output', nn.Linear(hidden_dim, output_dim))
         
     def forward(self, x, t):
@@ -20,4 +20,5 @@ class MLP(nn.Module):
     
     def step(self, x_t: torch.Tensor, t_start: torch.Tensor, t_end: torch.Tensor):
         t_start = t_start.view(1,1 ).expand(x_t.shape[0], 1)
+        # x + deltat * f(x, t_start + deltat / 2)
         return x_t + (t_end - t_start) * self(x_t + self(x_t, t_start) * (t_end - t_start) / 2, t_start + (t_end - t_start) / 2)
--- pep2prob/load_data.py
+++
@@ -1,4 +0,0 @@
-def load_data_generator():
-    from datasets import load_dataset
-    stream_data = load_dataset(path="data/Pep2Prob/data_split/train_test_split_set_1", streaming=True)
-    return stream_data
