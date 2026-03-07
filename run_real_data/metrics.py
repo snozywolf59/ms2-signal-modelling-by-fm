fc11f7998a841dcc08c45fd54cd4e45b8819bff1
Bang <thbang727@gmail.com>
2026-03-07 22:52:06 +0700

    new metrics

--- run_real_data/metrics.py
+++ run_real_data/metrics.py
@@ -4,51 +4,68 @@ import torch
 There are implementations of some metrics for flow matching model evaluation
 Have 3 metrics:
     - pcc:  Pearson Correlation Coefficient
-    - l1:   L1 
+    - l1:   L1
     - l2:   L2
     - sa:   SA
 Params:
     - intensity_1: (B, D)
     - intensity_2: (B, D)
-    
+
 Output:
     - (mean, max)
 
 """
 
 
-def pcc(intensity_1: torch.Tensor, intensity_2: torch.Tensor):
+def pcc(
+    intensity_1: torch.Tensor,
+    intensity_2: torch.Tensor,
+    mask: torch.Tensor = None,
+):
     eps = 1e-8
-    
+
     x = intensity_1
     y = intensity_2
-    
-    x = x - x.mean(dim=1, keepdim=True)
-    y = y - y.mean(dim=1, keepdim=True)
-    
-    cov = (x * y).sum(dim=1)
-    
-    std_x = torch.sqrt((x ** 2).sum(dim=1))
-    std_y = torch.sqrt((y ** 2).sum(dim=1))
-    pcc = cov / (std_x * std_y + eps)
-    
-    return pcc.mean().item(), pcc.min().item()
+    if mask is not None:
+        x = x * mask
+        y = y * mask
+    x_mean = x.mean(dim=1, keepdim=True)
+    y_mean = y.mean(dim=1, keepdim=True)
+    x_centered = x - x_mean
+    y_centered = y - y_mean
+    numerator = (x_centered * y_centered).sum(dim=1)
+    denominator = (
+        torch.sqrt((x_centered**2).sum(dim=1) * (y_centered**2).sum(dim=1))
+        + eps
+    )
+    pcc_values = numerator / denominator
+    return pcc_values.mean().item(), pcc_values.min().item()
+
 
 def l1(intensity_1: torch.Tensor, intensity_2: torch.Tensor):
     intensity_1 = (intensity_1 - intensity_2).abs()
-    return intensity_1.sum(dim=1).mean().item(), intensity_1.sum(dim=1).max().item()
+    return (
+        intensity_1.sum(dim=1).mean().item(),
+        intensity_1.sum(dim=1).max().item(),
+    )
+
 
 def l2(intensity_1: torch.Tensor, intensity_2: torch.Tensor):
     intensity_1 = (intensity_1 - intensity_2) ** 2
     return intensity_1.mean().item(), intensity_1.sum(dim=1).max().item()
 
-def sa(intensity_1: torch.Tensor, intensity_2: torch.Tensor):
-    dot_p = (intensity_1 * intensity_2).sum(dim=1)
-    dist_1 = intensity_1.pow(2).sum(dim=1).sqrt()
-    dist_2 = intensity_2.pow(2).sum(dim=1).sqrt()
-    cos = dot_p / (dist_1 * dist_2 + 1e-8)
-    cos = cos.clamp(-1.0, 1.0)
-    
-    sa_values = torch.acos(cos)
-    return sa_values.mean().item(), sa_values.max().item()
-    
+
+def sa(
+    intensity_1: torch.Tensor,
+    intensity_2: torch.Tensor,
+    mask: torch.Tensor = None,
+):
+    if mask is not None:
+        intensity_1 = intensity_1 * mask
+        intensity_2 = intensity_2 * mask
+
+    dot_product = (intensity_1 * intensity_2).sum(dim=1)
+    norm_1 = torch.sqrt((intensity_1**2).sum(dim=1))
+    norm_2 = torch.sqrt((intensity_2**2).sum(dim=1))
+    sa_values = dot_product / (norm_1 * norm_2 + 1e-8)
+    return sa_values.mean().item(), sa_values.min().item()
