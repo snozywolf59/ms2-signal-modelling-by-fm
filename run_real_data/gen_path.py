518e0ef57cad342e3ace287dcf3fe97b3008cb15
Bang <thbang727@gmail.com>
2026-03-08 14:55:59 +0700

    fix get x0

--- run_real_data/gen_path.py
+++ run_real_data/gen_path.py
@@ -1,13 +1,27 @@
 import torch
 
-def get_x0(x_1, max_scale=False):
+
+def get_x0(x_1: torch.Tensor, max_scale=False):
     # Sample x_0 from a distribution and scale based on the max value of itself
     # x_1 shape: (batch_size, length, 6)
     x_0 = torch.randn_like(x_1)
     if max_scale:
-        max_val = torch.max(torch.abs(x_0), dim=1, keepdim=True)[0]
-        x_0 = x_0 / (max_val + 1e-8)
+        if x_1.dim() == 1:
+            max_val = x_0.abs().max(keepdim=True)
+            max_val = torch.clamp(max_val, min=1e-8)
+            x_0 = x_0 / max_val
+        elif x_1.dim() == 2:  # (B, D)
+            max_val = x_0.abs().max(dim=1, keepdim=True)[0]
+
+            max_val = torch.clamp(max_val, min=1e-8)
+            x_0 = x_0 / max_val
+
+        elif x_1.dim() == 3:  # (B, L, D)
+            max_val = x_0.abs().amax(dim=(1, 2), keepdim=True)
+            max_val = torch.clamp(max_val, min=1e-8)
+            x_0 = x_0 / max_val
     return x_0
+
 
 def get_xt(x_0, x_1, t, sigma=0.001):
     noise = torch.randn_like(x_0)
