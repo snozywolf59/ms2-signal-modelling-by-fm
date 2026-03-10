8f3a99a03d5daffa42ce0978d148756545433baa
bangth <thbang727@gmail.com>
2026-03-10 08:16:27 +0100

    fix gen path

--- run_real_data/gen_path.py
+++ run_real_data/gen_path.py
@@ -44,6 +44,9 @@ def get_x0(x_1: torch.Tensor, max_scale=False):
 
 def get_xt(x_0, x_1, t, sigma=0.001):
     noise = torch.randn_like(x_0)
-    t = t.view(-1, 1, 1)
+    if noise.dim() == 3:
+        t = t.view(-1, 1, 1)
+    elif noise.dim() == 2:
+        t = t.view(-1, 1)
     x_t = (1 - t) * x_0 + t * x_1 + sigma * noise
     return x_t
